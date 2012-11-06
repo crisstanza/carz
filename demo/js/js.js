824b909360bfa12fdae7b8ae605670983cee2863
crisstanza <crisstanza@home>
2012-11-05 23:27:37 -0200

    Some movements

--- demo/js/js.js
+++ demo/js/js.js
@@ -1,8 +1,33 @@
 (function() {
 
+	var myGame = null;
+
+	var _LEFT = 37;
+	var _UP = 38;
+	var _RIGHT = 39;
+	var _DOWN = 40;
+
+	function doKeyDown(evt){
+		var key = evt.keyCode;
+		if ( key == _LEFT ) {
+			myGame.turn(-1);
+			evt.preventDefault();
+		} else if ( key == _UP ) {
+			myGame.move(1);
+			evt.preventDefault();
+		} else if ( key == _RIGHT ) {
+			myGame.turn(1);
+			evt.preventDefault();
+		} else if ( key == _DOWN ) {
+			myGame.move(-1);
+			evt.preventDefault();
+		}
+	}
+
 	function init() {
-		var myGame = new MyGame('my-canvas');
+		myGame = new MyGame('my-canvas');
 		myGame.start();
+		window.addEventListener('keydown', doKeyDown, true);
 	}
 
 	window.addEventListener('load', init, false);	
