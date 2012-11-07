05765bb6bd15809c0679473a0abc8fed5fd5ecad
crisstanza <crisstanza@home>
2012-11-06 22:48:09 -0200

    Some more movements and zoom

--- demo/js/MyGame.js
+++ demo/js/MyGame.js
@@ -1,5 +1,24 @@
 function MyGame(canvasId) {
 
+	MyGame._LEFT = 37;
+	MyGame._UP = 38;
+	MyGame._RIGHT = 39;
+	MyGame._DOWN = 40;
+
+	MyGame._1 = 49;
+	MyGame._2 = 50;
+	MyGame._3 = 51;
+	MyGame._4 = 52;
+	MyGame._5 = 53;
+	MyGame._6 = 54;
+	MyGame._7 = 55;
+	MyGame._8 = 56;
+	MyGame._9 = 57;
+
+	MyGame._Z = 90;
+	MyGame._X = 88;
+	MyGame._C = 67;
+
 	this.mainLoop = null;
 
 	this.canvasId = canvasId;
@@ -7,7 +26,7 @@ function MyGame(canvasId) {
 
 	this.offsetX = 10;
 	this.offsetY = 10;
-	this.scale = 4;
+	this.scale = 10;
 
 	this.lineWidth = 1;
 
@@ -19,6 +38,7 @@ function MyGame(canvasId) {
 		{
 			speed: 5,
 			angle: 0,
+			fillStyle: '#333',
 			lines: [
 				[20, 4, 40, 4, 40, 12, 20, 12, 20, 4],
 				[22, 3, 25, 3, 25, 4, 22, 4, 22, 3],
@@ -28,28 +48,31 @@ function MyGame(canvasId) {
 			]
 		},
 		{
-			speed: 0,
+			speed: 10,
 			angle: 0,
+			fillStyle: '#00A',
 			lines: [
-				[20, 16, 40, 16, 40, 24, 20, 24, 20, 16],
-				[22, 15, 25, 15, 25, 16, 22, 16, 22, 15],
+				[22, 16, 40, 16, 40, 24, 22, 24, 22, 16],
+				[24, 15, 27, 15, 27, 16, 24, 16, 24, 15],
 				[35, 15, 38, 15, 38, 16, 35, 16, 35, 15],
-				[22, 24, 25, 24, 25, 25, 22, 25, 22, 24],
+				[24, 24, 27, 24, 27, 25, 24, 25, 24, 24],
 				[35, 24, 38, 24, 38, 25, 35, 25, 35, 24],
 			]
 		},
 		{
-			speed: 0,
+			speed: 20,
 			angle: 0,
+			fillStyle: '#040',
 			lines: [
-				[20, 28, 40, 28, 40, 36, 20, 36, 20, 28],
-				[22, 27, 25, 27, 25, 28, 22, 28, 22, 27],
-				[35, 27, 38, 27, 38, 28, 35, 28, 35, 27],
-				[22, 36, 25, 36, 25, 37, 22, 37, 22, 36],
-				[35, 36, 38, 36, 38, 37, 35, 37, 35, 36],
+				[21, 28, 40, 28, 40, 35, 21, 35, 21, 28],
+				[25, 27, 28, 27, 28, 28, 25, 28, 25, 27],
+				[33, 27, 36, 27, 36, 28, 33, 28, 33, 27],
+				[25, 35, 28, 35, 28, 36, 25, 36, 25, 35],
+				[33, 35, 36, 35, 36, 36, 33, 36, 33, 35],
 			]
 		}
 	];
+	this.currentCar = 0;
 }
 
 MyGame.prototype.refresh = function() {
@@ -101,14 +124,14 @@ MyGame.prototype.refresh = function() {
 		context.strokeStyle = '#111';
 		context.lineWidth = 1;
 	}
-	{
+	for ( var h = 0 ; h < this.cars.length ; h++ ) {
+		var car = this.cars[h];
 		context.save();
-		context.rotate(this.cars[0].angle);
-		//
-		context.fillStyle = '#333';
+		context.rotate(car.angle);
+		context.fillStyle = car.fillStyle;
 		context.beginPath();
-		for ( var i = 0 ; i < this.cars[0].lines.length ; i++ ) {
-			var line = this.cars[0].lines[i];
+		for ( var i = 0 ; i < car.lines.length ; i++ ) {
+			var line = car.lines[i];
 			for ( var j = 0 ; j < line.length ; j++ ) {
 				if ( j == 0 ) {
 					context.moveTo(this.transformX(line[j]), this.transformY(line[++j]));
@@ -121,40 +144,6 @@ MyGame.prototype.refresh = function() {
 			context.stroke();
 		}
 		context.restore();
-	}
-	{
-		context.fillStyle = '#00A';
-		context.beginPath();
-		for ( var i = 0 ; i < this.cars[0].lines.length ; i++ ) {
-			var line = this.cars[1].lines[i];
-			for ( var j = 0 ; j < line.length ; j++ ) {
-				if ( j == 0 ) {
-					context.moveTo(this.transformX(line[j]), this.transformY(line[++j]));
-				} else {
-					context.lineTo(this.transformX(line[j]), this.transformY(line[++j]));
-				}
-			}
-			context.closePath();
-			context.fill();
-			context.stroke();
-		}
-	}
-	{
-		context.fillStyle = '#040';
-		context.beginPath();
-		for ( var i = 0 ; i < this.cars[0].lines.length ; i++ ) {
-			var line = this.cars[2].lines[i];
-			for ( var j = 0 ; j < line.length ; j++ ) {
-				if ( j == 0 ) {
-					context.moveTo(this.transformX(line[j]), this.transformY(line[++j]));
-				} else {
-					context.lineTo(this.transformX(line[j]), this.transformY(line[++j]));
-				}
-			}
-			context.closePath();
-			context.fill();
-			context.stroke();
-		}
 	}
 };
 
@@ -179,15 +168,70 @@ MyGame.prototype.stop = function() {
 };
 
 MyGame.prototype.move = function(multiplier) {
-	for ( var i = 0 ; i < this.cars[0].lines.length ; i++ ) {
-		var line = this.cars[0].lines[i];
+	var car = this.cars[this.currentCar];
+	for ( var i = 0 ; i < car.lines.length ; i++ ) {
+		var line = car.lines[i];
 		for ( var j = 0 ; j < line.length ; j++ ) {
-			line[j]+=(this.cars[0].speed*multiplier);
+			line[j]+=(car.speed*multiplier);
 			++j;
 		}
 	}
 };
 
 MyGame.prototype.turn = function(multiplier) {
-	this.cars[0].angle += (multiplier*0.01);
+	var car = this.cars[this.currentCar];
+	car.angle += (multiplier*0.01);
 };
+
+MyGame.prototype.doKeyDown = function(evt) {
+	var key = evt.keyCode;
+	if ( key == MyGame._LEFT ) {
+		this.turn(-1);
+		evt.preventDefault();
+	} else if ( key == MyGame._UP ) {
+		this.move(1);
+		evt.preventDefault();
+	} else if ( key == MyGame._RIGHT ) {
+		this.turn(1);
+		evt.preventDefault();
+	} else if ( key == MyGame._DOWN ) {
+		this.move(-1);
+		evt.preventDefault();
+	} else if ( key == MyGame._1 ) {
+		this.scale = 1;
+		evt.preventDefault();
+	} else if ( key == MyGame._2 ) {
+		this.scale = 2;
+		evt.preventDefault();
+	} else if ( key == MyGame._3 ) {
+		this.scale = 4;
+		evt.preventDefault();
+	} else if ( key == MyGame._4 ) {
+		this.scale = 8;
+		evt.preventDefault();
+	} else if ( key == MyGame._5 ) {
+		this.scale = 16;
+		evt.preventDefault();
+	} else if ( key == MyGame._6 ) {
+		this.scale = 32;
+		evt.preventDefault();
+	} else if ( key == MyGame._7 ) {
+		this.scale = 48;
+		evt.preventDefault();
+	} else if ( key == MyGame._8 ) {
+		this.scale = 64;
+		evt.preventDefault();
+	} else if ( key == MyGame._9 ) {
+		this.scale = 96;
+		evt.preventDefault();
+	} else if ( key == MyGame._Z ) {
+		this.currentCar = 0;
+		evt.preventDefault();
+	} else if ( key == MyGame._X ) {
+		this.currentCar = 1;
+		evt.preventDefault();
+	} else if ( key == MyGame._C ) {
+		this.currentCar = 2;
+		evt.preventDefault();
+	}
+}
--- demo/js/js.js
+++ demo/js/js.js
@@ -2,53 +2,10 @@
 
 	var myGame = null;
 
-	var _LEFT = 37;
-	var _UP = 38;
-	var _RIGHT = 39;
-	var _DOWN = 40;
-
-	var _1 = 49;
-	var _2 = 50;
-	var _3 = 51;
-	var _4 = 52;
-	var _5 = 53;
-
-	function doKeyDown(evt){
-		var key = evt.keyCode;
-		if ( key == _LEFT ) {
-			myGame.turn(-1);
-			evt.preventDefault();
-		} else if ( key == _UP ) {
-			myGame.move(1);
-			evt.preventDefault();
-		} else if ( key == _RIGHT ) {
-			myGame.turn(1);
-			evt.preventDefault();
-		} else if ( key == _DOWN ) {
-			myGame.move(-1);
-			evt.preventDefault();
-		} else if ( key == _1 ) {
-			myGame.scale = 1;
-			evt.preventDefault();
-		} else if ( key == _2 ) {
-			myGame.scale = 2;
-			evt.preventDefault();
-		} else if ( key == _3 ) {
-			myGame.scale = 4;
-			evt.preventDefault();
-		} else if ( key == _4 ) {
-			myGame.scale = 8;
-			evt.preventDefault();
-		} else if ( key == _5 ) {
-			myGame.scale = 16;
-			evt.preventDefault();
-		}
-	}
-
 	function init() {
 		myGame = new MyGame('my-canvas');
 		myGame.start();
-		window.addEventListener('keydown', doKeyDown, true);
+		window.addEventListener('keydown', function(evt) { myGame.doKeyDown(evt); }, true);
 	}
 
 	window.addEventListener('load', init, false);	
