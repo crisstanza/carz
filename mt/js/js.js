0bbf68b82fb6d4e572ecb7508dd868eb88ff765d
crisstanza <crisstanza@home>
2012-11-18 22:21:18 -0200

    Buttons

---
+++ mt/js/js.js
@@ -0,0 +1,20 @@
+(function() {
+
+	var rows = 12;
+	var cols = 29;
+
+	function init() {
+		var myForm = document.forms['my-form'];
+		var sb = [];
+		for ( var i = 0 ; i < rows ; i++ ) {
+			for ( var j = 0 ; j < cols ; j++ ) {
+				sb.push('<input name="point" type="checkbox" />');
+			}
+			sb.push('<br />');
+		}
+		myForm.innerHTML = sb.join('');
+	}
+
+	window.addEventListener('load', init, false);	
+
+})();
